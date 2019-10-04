a99bd9a12f2e43e9fba7912d58b8e117cad59a44
katka6622@gmail.com <katka6622@gmail.com>
2019-10-04 15:32:46 +0500

    формирование данных в тестах

--- UnitTestProject3/UnitTest1.cs
+++ UnitTestProject3/UnitTest1.cs
@@ -6,9 +6,13 @@ namespace UnitTestProject3
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Метод проверяет изменение баланса при покупке
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodCalcBalance()
         {
+            DataModel Data = new DataModel();//Генерация модели данных
             //test
         }
     }
