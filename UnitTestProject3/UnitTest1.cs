d74a9df40b5bb1fb2d5368a5b221780b76b3a761
katka6622@gmail.com <katka6622@gmail.com>
2019-10-04 15:42:36 +0500

    Тест метод проверяет баланс пользователя при покупке самолета

--- UnitTestProject3/UnitTest1.cs
+++ UnitTestProject3/UnitTest1.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FlyMain.Data;
+using FlyMain.Model;
+using FlyMain.ViewModel;
+using System.Linq;
+
 namespace UnitTestProject3
 {
     [TestClass]
@@ -13,7 +17,20 @@ namespace UnitTestProject3
         public void TestMethodCalcBalance()
         {
             DataModel Data = new DataModel();//Генерация модели данных
-            //test
+            AirplaneModel airplane = Data.AirplaneList.First(); // берем первый самолет
+            int price = airplane.Price;//берем стоимость самолета
+            int balance = Data.Ballance;//берем баланс пользователя
+            ShoopViewModel svm = new ShoopViewModel(Data);//Создали класс, отвечающий за покупку
+
+            svm.ByAirplane(airplane.uid);
+            Assert.IsNotNull(airplane);//проверка наличия самолета
+
+            Assert.AreEqual(balance - price, svm.Data.Ballance);//проверка балланса
+
+            svm.Data.Ballance = 10;
+            svm.ByAirplane(airplane.uid);
+            Assert.AreEqual(10, svm.Data.Ballance);//проверка балланса когда балланс меньше цены
+
         }
     }
 }
